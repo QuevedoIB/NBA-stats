5bb205a5f3dd0b0975e5663bd98d01fd81feaaad
Ivan <4esoquevedoi@gmail.com>
2021-10-14 00:11:32 +0200

    Refactor searchplayer initial setup

--- src/pages/PlayerDetail/index.js
+++ src/pages/PlayerDetail/index.js
@@ -1,16 +1,68 @@
+import { useState, useCallback, useMemo } from "react";
 import { useParams } from "react-router";
 
+import SearchBar from "components/SearchBar";
+
 import usePlayers from "hooks/usePlayers";
+import usePlaceHolderSource from "hooks/usePlaceholderSource";
+
+import PlaceholderImage from "public/images/player-placeholder.png";
+
+import styles from "./PlayerDetail.module.css";
 
 const PlayerDetail = () => {
+  const [searchPlayer, setSearchPlayer] = useState("");
   const { playerId } = useParams();
   const {
     filteredPlayers: [player],
   } = usePlayers({ key: "personId", value: playerId });
+
+  const { src, loaded: loadedImage } = usePlaceHolderSource({
+    src: `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${player.personId}.png`,
+    placeHolderSrc: PlaceholderImage,
+  });
+
+  const suggestions = useMemo(() => [], [searchPlayer]);
+
+  const updateSearchedPlayer = useCallback(({ target: { value } }) => {
+    setSearchPlayer(value);
+  }, []);
+
+  const onSelectSuggestedPlayer = useCallback(({ temporaryDisplayName }) => {
+    setSearchPlayer(temporaryDisplayName);
+  }, []);
+
   console.log(player);
   return (
-    <section>
-      <h3>{player?.temporaryDisplayName}</h3>
+    <section className={styles.container}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          width: "fit-content",
+        }}
+      >
+        <h3>{player?.temporaryDisplayName}</h3>
+        <img
+          src={src}
+          alt={`${player.temporaryDisplayName}`}
+          className={`${styles.image} ${
+            loadedImage ? "" : styles.placeholderImage
+          }`}
+          loading="lazy"
+        />
+      </div>
+      <div>
+        <SearchBar
+          searchText={searchPlayer}
+          suggestions={suggestions}
+          keyword="temporaryDisplayName"
+          onSuggestionClick={onSelectSuggestedPlayer}
+          onSearchChange={updateSearchedPlayer}
+        />
+      </div>
     </section>
   );
 };
